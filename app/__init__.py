1df1449b97c8c64252f97332fffbf25b778b5af6
Ubuntu <ubuntu@ip-172-31-31-60.ec2.internal>
2026-07-21 09:07:40 +0000

    Add Prometheus/Grafana monitoring stack with Flask metrics instrumentation

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from app.config import Config
-
+from prometheus_flask_exporter import PrometheusMetrics
 db = SQLAlchemy()
 login_manager = LoginManager()
 
@@ -10,6 +10,7 @@ login_manager = LoginManager()
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    metrics = PrometheusMetrics(app)
 
     db.init_app(app)
     login_manager.init_app(app)
@@ -22,7 +23,6 @@ def create_app(config_class=Config):
     app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
     app.register_blueprint(api_bp, url_prefix="/api")
-
     with app.app_context():
         db.create_all()
 
